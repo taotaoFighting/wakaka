7eda2ea13664fa5df70cd4fe1528c50d40698a65
apple <15093455345@163.com>
2016-12-17 14:13:14 +0800

    commit20161217

---
+++ wakaka/BaseConfig/HeaderPch.swift
@@ -0,0 +1,15 @@
+//
+//  HeaderPch.swift
+//  wakaka
+//
+//  Created by apple on 16/12/15.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+import UIKit
+
+let KS_Width = UIScreen.main.bounds.size.width
+
+let KS_Height = UIScreen.main.bounds.size.height
+
+let indentifier = "firstCell"
--- wakaka/Controllers/FirstPageController/FirstViewController.swift
+++ wakaka/Controllers/FirstPageController/FirstViewController.swift
@@ -8,16 +8,60 @@
 
 import UIKit
 
-class FirstViewController: BaseViewController {
+class FirstViewController: BaseViewController ,UITableViewDelegate,UITableViewDataSource{
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationItem.title = "首页"
         
-        // Do any additional setup after loading the view.
-    }
+        self.view .addSubview(self.mainTableView)
+        
 
+    }
+    
+    lazy var mainTableView: UITableView = {
+        
+        let mainTableView:UITableView = UITableView.init(frame:CGRect.init(x: 0, y: 0, width: KS_Width, height: KS_Height) , style: UITableViewStyle.plain)
+        
+        mainTableView.delegate = self
+        
+        mainTableView.dataSource = self
+        
+        mainTableView.register(UINib.init(nibName: "FirstTableViewCell", bundle: nil), forCellReuseIdentifier: indentifier)
+        
+        return mainTableView
+    }()
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell:FirstTableViewCell = tableView.dequeueReusableCell(withIdentifier: indentifier) as! FirstTableViewCell
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 100
+    }
+    
+    
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        let point:CGPoint = scrollView.contentOffset
+        
+        NSLog("point = (%lu,%lu)", point.x,point.y)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
