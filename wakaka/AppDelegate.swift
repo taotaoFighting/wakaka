b3885c96d16ecd951c24673e688cd3e6adcded7c
taotaoFighting <15093455345@163.com>
2016-12-26 18:12:28 +0800

    test

--- wakaka/AppDelegate.swift
+++ wakaka/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
-        print("你是头猪吗")
+        print("你是头猪吗");
         print("你是头猪吗");
         print("你是头猪吗")
 
